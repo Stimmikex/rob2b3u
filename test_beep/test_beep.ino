fd16204ef47d4165eb8dfe7d54b773d3eb477e7d
Stimmikex <styrmir97@gmail.com>
2017-02-01 10:09:02 +0000

    01/02/2017

--- test_beep/test_beep.ino
+++ test_beep/test_beep.ino
@@ -8,7 +8,7 @@
 
 *******/
 int buzzer = 10;
-int smokeA5 = A5;
+int smokeA0 = A0;
 int humi = 2;
 
 #define DHTPIN 2
@@ -19,22 +19,22 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
 
   pinMode(buzzer, OUTPUT);
-  pinMode(smokeA5, INPUT);
+  pinMode(smokeA0, INPUT);
   Serial.begin(9600);
   dht.begin();
 }
 
 void loop() {
   // MQ-7 sensor
-  if(smokeA5 > 500) {
-     tone(buzzer, 1000, 200);
-     delay(1000);
-  }
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius
   float t = dht.readTemperature();
-  int MQ = analogRead(smokeA5);
+  int MQ = analogRead(smokeA0);
+  /*if(MQ > 500) {
+     tone(buzzer, 1000, 200);
+     delay(1000);
+  }*/
 
   Serial.print("Humidity: "); 
   Serial.print(h);
